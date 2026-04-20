ea8532e173fbff149582829cf58e114184b49c68
Candle-Electric <109983451+Candle-Electric@users.noreply.github.com>
2026-04-19 23:59:02 -0500

    Update Gameplay.asm
    Adding Height Detection Detail For Regex Collision Calculation With The Chunk Offset Check.

--- src/Gameplay.asm
+++ src/Gameplay.asm
@@ -209,6 +209,7 @@ Gameplay_Loop:
     mov #1, collision_flags
 .Alternate_Collision
     bn boulder_chunk_offset, 1, .Collision_Done ; Skip Check For Collision When Not In Boulder Range.
+    ld player_sprite_y
 .Collision_Done
 
 .Handle_Stun_Timer
