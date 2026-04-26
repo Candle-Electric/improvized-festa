6fd8258435267e2f702a08832637b4060079aa4f
Candle-Electric <109983451+Candle-Electric@users.noreply.github.com>
2026-04-26 00:53:33 -0400

    Update Gameplay.asm
    HWDJJFREGBA!

--- src/Gameplay.asm
+++ src/Gameplay.asm
@@ -211,7 +211,7 @@ Gameplay_Loop:
     bn boulder_chunk_offset, 1, .Collision_Done ; Skip Check For Collision When Not In Boulder Range.
     ; L.D. Obstacle Type Here And Determine Jump Path 
     ld player_sprite_y
-    sub #16
+    sub #17
     bn acc, 7, Collision_Done
 .Collision_Done
 
