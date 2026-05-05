aa2d07790906070a79a8672f67e9e94d2f240ad1
Candle-Electric <109983451+Candle-Electric@users.noreply.github.com>
2026-05-04 21:58:04 -0700

    Update Gameplay.asm
    Architecting Out More PseudoCode Checking Tries Re Trtying 4 Options Working. HSWD!

--- src/Gameplay.asm
+++ src/Gameplay.asm
@@ -224,6 +224,8 @@ Gameplay_Loop:
 	ld Collision_Flags
 	; bnz .Player_State_Collision_Done_Set
 	; Instaniate A Player_Stunned Variable, And Have The Penguin Hop In Place When Stunned?
+	mov #1, Player_Stunned
+	; *Then, Possibly Moveing this to the Player Control Section, If Player_Stunned = 1/"Y," they'll bounce left/right on an Oscillator and up+down on the Frame Counter.
 
 .Handle_Stun_Timer
 	; bp player_stunned_bool, 0, .Draw_Screen
