51c4d2340bf9d642f52a63a52596109ae127dff8
Candle-Electric <109983451+Candle-Electric@users.noreply.github.com>
2026-05-10 22:36:59 -0700

    Refactor Gameplay.ASM For Clarity And Organization
    Added A Note For Moving Some Of Yesterday's PseudoCode. Happy Mothers' Day, H.B.D.N.!

--- src/Gameplay.asm
+++ src/Gameplay.asm
@@ -219,7 +219,7 @@ Gameplay_Loop:
     mov #11, stun_timer
     ; mov #0, Stun_Direction ; Meaning Left. Consider A 1 Variable Of 8 Flags.
     ; Once Grounded, Switch Directions And Keep Bouncing And Setting Bits Until Flag_Var = 255
-    ; bp Flag_Bit (Direction)
+    ; bp Flag_Bit (Direction) {When Landed, Move this Line Near .Stun_Drop + Do A "Grounded" Check.}/.
 .Cillision_HighBoulder ; Let's Pretend For Now.
     ; Done with the L.D.C.?
 .Collision_Done
