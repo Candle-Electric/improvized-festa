1ab786c7e21149ba3eefeba448e8a1a97dbd5c87
Candle-Electric <109983451+Candle-Electric@users.noreply.github.com>
2026-05-26 23:54:58 -0500

    Update Gameplay.asm
    Planning Variable Sorted PseudoCode.

--- src/Gameplay.asm
+++ src/Gameplay.asm
@@ -225,7 +225,7 @@ Gameplay_Loop:
 	; Once Done Bouncing And Changing Directions, Restore Control.
 	; Flag Set. Skip All Other Collision In The Meantime?
     ; 7 6 5 4 3 2 1 0 ; Set, Direction., {.Up Timer.?} , [ . . Timer . . ]
-.Cillision_HighBoulder ; Let's Pretend For Now.
+.Cillision_HighBoulder ; Let's Pretend For Now. ; This time, it'll be Y < 8.
     ; Done with the L.D.C.?
 .Collision_Done
 
