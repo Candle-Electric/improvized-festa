a4ecf0f9805109fbe49f8d4bf559e8c0f49995b2
Candle-Electric <109983451+Candle-Electric@users.noreply.github.com>
2026-06-30 21:38:01 -0700

    Update Gameplay.asm
    Adding In Sprite Addresses, Pending Names For The Stun Sprites. TUP,R,RW,BDRB,APAAAF!

--- src/Gameplay.asm
+++ src/Gameplay.asm
@@ -298,7 +298,8 @@ Gameplay_Loop:
 	; Different For Rising Vs. Bouncing Back + Forth?
 	ld player_stunned_bool
     bnz .Jumping ; / Standing, Note this should Go Before The others.
-	mov #
+	mov #<Penguin
+	mov #>
 .Jumping
 	bp player_grounded_bool, 0, .Draw_Trot_L
 	mov #<Penguin_Jump_Up_Mask, player_sprite_sprite_address
