f473d7f16f486efcd88204f076e6137f987dcdb6
Candle-Electric <109983451+Candle-Electric@users.noreply.github.com>
2026-08-06 21:00:24 -0500

    Update Gameplay.asm
    Keeping Operatives Under The Set Variables, Seeking Count Jumps.

--- src/Gameplay.asm
+++ src/Gameplay.asm
@@ -248,6 +248,7 @@ Gameplay_Loop:
 	mov #128, StunFlags ; Leftmost Flag = Direction
     mov #3, stun_rise_timer ; Actually, we'll Want To Dec this Timer Outside Of Here; Only _Set_ it Here. ; dec stun_rise_timer ; Initial Hop. -1 Y, Then 0 &And +1 Y Once it's Done.
     ; ld stun_rise_timer
+    ; mov #8, stun_timer
     ; bz .Stun_Hop ; Move Up.!
 	; jmpf .Stun_Drop
 	inc Player_Sprite_Y
